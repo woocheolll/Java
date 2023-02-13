d7d19e1d09a5c7efe87de82a42e2c10b6b5698d1
woocheolll <woocheol2868@gmail.com>
2023-02-13 16:35:00 +0900

    조건 연산자와 대입 연산자

--- Operator.java
+++ Operator.java
@@ -281,3 +281,31 @@ class Operator7_2 {
         System.out.println(">>> 연산자에 의한 결과 = " + (num2 >>> 2)); // 1073741822
     }
 }
+
+class Operator8_1 {
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 7;
+        int result;
+
+        result = num1 - num2 > 0 ? num1 : num2;
+
+        System.out.println("두 정수 중 더 큰 수는 " + result + "입니다."); // 7
+    }
+}
+
+class Operator8_2 {
+    public static void main(String[] args) {
+        int num1 = 7, num2 = 7, num3 = 7;
+
+        num1 = num1 - 3;
+        num2 -= 3;
+        num3 =- 3;
+
+        System.out.println("- 연산자에 의한 결과 : "+ num1); // 4
+        System.out.println("-= 연산자에 의한 결과 : "+ num2); // 4
+
+        // = 위치를 주의하셔야 합니다.
+        // num3 =- 3; 는 num3 = -3 입니다.
+        System.out.println("=- 연산자에 의한 결과 : "+ num3); // -3
+    }
+}
