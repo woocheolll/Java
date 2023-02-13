c4e72a56ce16e3463d3171115ce13772a6de4c23
woocheolll <woocheol2868@gmail.com>
2023-02-13 14:47:55 +0900

    증감 연산자와 부호 연산자

---
+++ Operator.java
@@ -0,0 +1,42 @@
+class Operator2_1 {
+    public static void main(String[] args) {
+        int k = 0, j = 3;
+
+        // 후위 형
+        k = j++;
+        System.out.println("k = j++;, k = " + k);
+        System.out.println("k = j++;, j = " + j);
+        // 다르게 표현 가능
+//        k = j;
+//        j++;
+//        System.out.println("k = j++;, k = " + k);
+//        System.out.println("k = j++;, j = " + j);
+
+        // 값 초기화
+        k = 0;
+        j = 3;
+
+        // 전위 형
+        k = ++j;
+        System.out.println("k = ++j;, k = " + k);
+        System.out.println("k = ++j;, j = " + j);
+        // 다르게 표현 가능
+//        ++j;
+//        k = j;
+//        System.out.println("k = ++j;, k = " + k);
+//        System.out.println("k = ++j;, j = " + j);
+
+    }
+}
+
+class Operator2_2 {
+    public static void main(String[] args) {
+        int i = -10;
+        i = +i;
+        System.out.println("i = +i; = " + i);
+
+        i = -10;
+        i = -i;
+        System.out.println("i = -i; = " + i);
+    }
+}
