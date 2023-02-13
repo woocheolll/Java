fe4e877cf79316cb73c9d3d18d202930adc1628c
woocheolll <woocheol2868@gmail.com>
2023-02-13 16:17:53 +0900

    비교 연산자와 문자열의 비교

--- Operator.java
+++ Operator.java
@@ -186,3 +186,43 @@ class Operator5_3 {
         System.out.println("x % z = " + x % z); // % z = 1
     }
 }
+
+class Operator6_1 {
+    public static void main(String[] args) {
+        int n1 = 10, n2 = 6;
+        char c1 = 'A', c2 = 'B';
+
+        System.out.print("n1 >= n2 = " ); // true
+        System.out.println(n1 >= n2);
+        System.out.print("n1 <= n2 = " ); // false
+        System.out.println(n1 <= n2);
+
+        System.out.print("n1 == n2 = " ); // false
+        System.out.println(n1 == n2);
+        System.out.print("n1 != n2 = " ); // true
+        System.out.println(n1 != n2);
+
+        // 산술변환 규칙에 의해서 char 타입이 int 타입으로 변환되어 연산됨
+        System.out.print("c1 < c2 = "); // true
+        System.out.println(c1 < c2); // 65 < 66
+        System.out.print("c1 > c2 = "); // false
+        System.out.println(c1 > c2); // 65 > 66
+    }
+}
+
+class Operator6_2 {
+    public static void main(String[] args) {
+        String s1 = "사랑";
+        String s2 = "사랑";
+
+        System.out.print("s1 == s2 = "); // true
+        System.out.println(s1 == s2);
+        System.out.println("s1.equals(s2) = " + s1.equals(s2)); // true
+
+        // 하지만!
+        String s3 = new String("사랑");
+        System.out.print("s1 == s3 = "); // false
+        System.out.println(s1 == s3);
+        System.out.println("s1.equals(s3) = " + s1.equals(s3)); // true
+    }
+}
