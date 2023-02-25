6c85028c348ec5a186f08f6914cc6b0f176e92e6
woocheolll <woocheol2868@gmail.com>
2023-02-25 14:03:12 +0900

    SA

--- Bus/Transport.java
+++ Bus/Transport.java
@@ -14,7 +14,16 @@ public class Transport{
         System.out.println("요금확인: "+ bus1.people*bus1.money);
         bus1.oil -= 50;
         System.out.println("주유량: "+ bus1.oil);
-        System.out.println("최대 승객수를 초과했습니다.");
+        bus1.state ="차고지행";
+        System.out.println("상태:"+ bus1.state);
+        bus1.oil += 10;
+        System.out.println("주유량: "+ bus1.oil);
+        bus1.people = 45;
+        bus1.allpeople += bus1.people;
+        if ( bus1.maxpeople < bus1.allpeople) {
+            System.out.println("최대 승객수를 초과했습니다.");
+        }
+        bus1.allpeople -= bus1.people;
         bus1.people = 5;
         bus1.allpeople += bus1.people;
         System.out.println("탑승 승객 수: "+  bus1.people);
@@ -24,6 +33,7 @@ public class Transport{
         bus1.oil -= 45;
         System.out.println("주유량:"+  bus1.oil);
         System.out.println("주유가 필요합니다");
+        bus1.state ="운행";
         System.out.println("상태:"+ bus1.state);
 
     }
